1c457a750e8d17481eb7fa6c758783b2211afaf1
Dominique Hazael-Massieux <dom@w3.org>
2017-07-04 17:26:48 +0200

    Improve readability a bit

--- w3c-ipr.js
+++ w3c-ipr.js
@@ -1,7 +1,7 @@
 var async = require("async")
 ;
 
-var fromUrlToId = function(url) { return url.replace(/.*\//, "");};
+const fromUrlToId = url => url.replace(/.*\//, "");
 
 module.exports = function iprcheck(w3c, w3cprofileid, name, w3cgroupids, store, cb) {
     async.map(
@@ -35,20 +35,18 @@ module.exports = function iprcheck(w3c, w3cprofileid, name, w3cgroupids, store, 
                                    .fetch({embed: true}, function(err, participations) {
                                        if (err) return groupcb(err);
                                        var orgids = participations
-                                           .filter(function(p) { return !p.individual;})
-                                           .map(function(p) { return fromUrlToId(p._links.organization.href);});
+                                           .filter(p => !p.individual)
+                                           .map(p => fromUrlToId(p._links.organization.href));
                                        w3c.user(w3cprofileid)
                                            .affiliations()
                                            .fetch(function(err, affiliations) {
                                                if (err) return groupcb(err);
 
-                                               var affids = affiliations.map(function(a) { return fromUrlToId(a.href);});
-                                               var intersection = orgids.filter(function(id) {
-                                                   return affids.indexOf(id) !== -1;
-                                               });
+                                               var affids = affiliations.map(a => fromUrlToId(a.href));
+                                               var intersection = orgids.filter(id =>  affids.includes(id));
                                                if (intersection.length) {
                                                    var affiliationId = intersection[0];
-                                                   var affiliationName = affiliations.filter(function(a) { return a.href == "https://api.w3.org/affiliations/" + affiliationId;})[0].title;
+                                                   var affiliationName = affiliations.find(a => a.href == "https://api.w3.org/affiliations/" + affiliationId).title;
                                                    return groupcb(null, {ok: true, affiliation: {id: affiliationId, name: affiliationName}});
                                                }
                                                return groupcb(null, {ok: false});

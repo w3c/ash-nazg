26b4b6926de6794b0131662f42cb9e070d81d602
Denis Ah-Kang <denis@w3.org>
2019-03-15 12:01:37 +0400

    winston no longer has a constructor

--- log.js
+++ log.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
             );
         }
 
-        logger = new (winston.Logger)({ transports: transports });
+        logger = winston.createLogger({ transports: transports });
     }
     return logger;
 };

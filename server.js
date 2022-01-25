87ad25b385cce8f1a25c6cd567a06273064a0846
Denis Ah-Kang <deniak@users.noreply.github.com>
2022-01-25 08:39:14 +0400

    Force disconnection for users with old format tokens (#241)
    * if a user has a legacy token, force them to reconnect
    
    * test to make sure users with old tokens gets disconnected
    
    * Update server.js
    
    Co-authored-by: Dominique Hazael-Massieux <dom@w3.org>
    
    Co-authored-by: Dominique Hazael-Massieux <dom@w3.org>

--- server.js
+++ server.js
@@ -163,6 +163,11 @@ router.get("/api/logout", function (req, res) {
 
 // check if the user is logged in
 router.get("/api/logged-in", function (req, res) {
+    // Format of github OAuth access tokens per https://github.blog/changelog/2021-03-31-authentication-token-format-updates-are-generally-available/
+    if (req.user && req.user.accessToken && !req.user.accessToken.startsWith("gho_")) {
+        log.info(`Force disconnect of ${req.user.username}`);
+        req.logout();
+    }
     res.json({ ok: req.isAuthenticated(), login: req.user ? req.user.username : null, admin: req.user ? req.user.admin : false });
 });
 

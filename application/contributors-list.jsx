b990847cc6773cadf8aa1ceea4224fbb52bac004
Denis Ah-Kang <denis@w3.org>
2025-12-12 16:08:18 +0400

    contributors list: add link to the PR in the repo manager

--- application/contributors-list.jsx
+++ application/contributors-list.jsx
@@ -1,6 +1,7 @@
 
 import React from "react";
 import Spinner from "../components/spinner.jsx";
+import { Link } from "react-router";
 
 require("isomorphic-fetch");
 let utils = require("./utils")
@@ -64,7 +65,7 @@ export default class ContributorsList extends React.Component {
                                             <td>
                                                 <ul>
                                                     {st.substantiveContributors[i].prs.map((pr) => {
-                                                        return <li key={pr.num}><a href={`https://github.com/${st.owner}/${st.shortName}/pull/${pr.num}`}>PR #{pr.num}</a> (closed on {pr.lastUpdated})</li>
+                                                        return <li key={pr.num}><a href={`https://github.com/${st.owner}/${st.shortName}/pull/${pr.num}`}>PR #{pr.num}</a> (closed on <a href={`${pp}pr/id/${st.owner}/${st.shortName}/${pr.num}`}>{pr.lastUpdated}</a>)</li>
                                                     })}
                                                 </ul>
                                             </td>
